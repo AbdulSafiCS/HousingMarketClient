a948ca2db9dd259f1b1aeed754cdee64c2c711ce
Abdul Safi <hasibsaf90@gmail.com>
2024-11-02 12:41:19 -0700

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CitiesComponent } from './cities/cities.component';
+import { ListingsComponent } from './listings/listings.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent }, // HomeComponent as the default
+  { path: 'cities', component: CitiesComponent },
+  { path: 'listings', component: ListingsComponent },
+];
+
+export const routing = RouterModule.forRoot(routes);
